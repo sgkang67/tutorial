95b17ea603961e0c3063b73e5d8dc4b1842a5c71
SeongguKang <sgkang67@gmail.com>
2018-05-20 13:54:51 +0900

    modify first.py at new-branch

--- first.py
+++ first.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+Amp = 5
+noData = 500
+t0 = 0
+t1 = 10
+tau = -0.1
 
-x = np.linspace(0, 10, 500)
-y = 5*np.exp(-0.1*x)*np.sin(x)
+x = np.linspace(t0, t1, noData)
+y = Amp*np.exp(tau*x)*np.sin(x)
 
 noise = np.random.randn(*y.shape)
 yn = y + noise
@@ -11,3 +16,8 @@ yn = y + noise
 plt.plot(x, y, x, yn)
 plt.show()
 
+y1 = Amp/2*np.cos(2*x)
+y1n = yn + y1
+
+plt.plot(x, y1n)
+plt.show()
