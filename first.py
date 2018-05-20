9ab209636ffd7bc7b7f87779c506f5634845c50b
SeongguKang <sgkang67@gmail.com>
2018-05-20 12:19:31 +0900

    yn error correction

--- first.py
+++ first.py
@@ -2,13 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x = np.linspace(0, 10, 100)
-y = np.exp(x)*np.sin(x)
+x = np.linspace(0, 10, 500)
+y = 5*np.exp(-0.1*x)*np.sin(x)
 
-plt.plot(x, y)
+noise = np.random.randn(*y.shape)
+yn = y + noise
+
+plt.plot(x, y, x, yn)
 plt.show()
-
-noise = np.random.randn(y.shape)
-yn = y+noise
-
 
